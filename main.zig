427f45d81c623d1f2d972eca7ed7429385d563b6
Christoffer Arvidsson <christoffer@arvidson.nu>
2025-08-23 10:14:59 +0200

    Refactor into HitRecord class

--- main.zig
+++ main.zig
@@ -1,9 +1,62 @@
 const std = @import("std");
 const stbiw = @import("stb_image_write");
 
-extern fn launch_raycast(img: [*]u8, width: c_int, height: c_int) void;
+extern fn launch_raycast(img: [*]u8, width: c_int, height: c_int) ?void;
 
 const vec3f = @Vector(3, f32);
+
+const HitRecord = struct {
+    point: vec3f,
+    normal: vec3f,
+    t: f32,
+    front_face: bool,
+
+    pub fn set_face_normal(self: *HitRecord, ray: *const Ray, outward_normal: vec3f) void {
+        self.front_face = vec3f_dot(ray.dir, outward_normal) < 0.0;
+        self.normal = if (self.front_face) outward_normal else -outward_normal;
+    }
+};
+
+const Sphere = struct {
+    center: vec3f,
+    radius: f32,
+
+    fn hit(self: *const Sphere, ray: *const Ray) ?HitRecord {
+        const oc = self.center - ray.origin;
+        const a = vec3f_dot(ray.dir, ray.dir);
+        const h = vec3f_dot(ray.dir, oc);
+        const c = vec3f_dot(oc, oc) - self.radius * self.radius;
+        const disc = h * h - a * c;
+
+        if (disc < 0.0) {
+            return null;
+        }
+
+        const sqrtd = @sqrt(disc);
+        var root = (h - sqrtd) / a;
+        if ((root <= RAY_TMIN) or (RAY_TMAX <= root)) {
+            root = (h + sqrtd) / a;
+            if ((root < RAY_TMIN) or (RAY_TMAX <= root)) {
+                return null;
+            }
+        }
+
+        const point = ray.at(root);
+        var record = HitRecord {
+            .t = root,
+            .point = point,
+            .normal = .{0.0, 0.0, 0.0},
+            .front_face = false,
+        };
+        const outward_normal = (point - self.center) / @as(vec3f, @splat(self.radius));
+        record.set_face_normal(ray, outward_normal);
+
+        return record;
+    }
+};
+
+const RAY_TMIN = 0.0;
+const RAY_TMAX = 1.0;
 
 const Ray = struct {
     origin: vec3f,
@@ -28,32 +81,20 @@ fn vec3f_dot(a: vec3f, b: vec3f) f32 {
     return c[0] + c[1] + c[2];
 }
 
-fn hit_sphere(center: vec3f, radius: f32, ray: *const Ray) f32 {
-    const oc = center - ray.origin;
-    const a = vec3f_dot(ray.dir, ray.dir);
-    const b = -2.0 * vec3f_dot(ray.dir, oc);
-    const c = vec3f_dot(oc, oc) - radius * radius;
-    const disc = b * b - 4.0 * a * c;
+fn ray_color(ray: *const Ray, sphere: *const Sphere) vec3f {
+    const hit = sphere.hit(ray);
 
-    if (disc < 0.0) {
-        return -1.0;
-    } else {
-        return (-b - @sqrt(disc)) / (2.0 * a);
-    }
-}
-
-fn ray_color(ray: *const Ray) vec3f {
-    const t = hit_sphere(vec3f{0.0, 0.0, -1.0}, 0.5, ray);
-    if (t > 0.0) {
-        const hit_normal = vec3f_normalize(ray.at(t) - vec3f{0.0, 0.0, -1.0});
-        return @as(vec3f, @splat(0.5)) * (hit_normal + @as(vec3f, @splat(1.0)));
+    if (hit != null) {
+        if (hit.?.t > 0.0) {
+            const hit_normal = vec3f_normalize(ray.at(hit.?.t) - vec3f{ 0.0, 0.0, -1.0 });
+            return @as(vec3f, @splat(0.5)) * (hit_normal + @as(vec3f, @splat(1.0)));
+        }
     }
     const unit_dir = vec3f_normalize(ray.dir);
     const a = 0.5 * (unit_dir[1] + 1.0);
     const a_v = @as(vec3f, @splat(a));
-    return @as(vec3f, @splat(1.0 - a)) * @as(vec3f, @splat(1.0)) + a_v*vec3f{0.5, 0.7, 1.0};
+    return @as(vec3f, @splat(1.0 - a)) * @as(vec3f, @splat(1.0)) + a_v * vec3f{ 0.5, 0.7, 1.0 };
 }
-
 
 pub fn main() !void {
     var allocator = std.heap.page_allocator;
@@ -80,23 +121,25 @@ pub fn main() !void {
     const viewport_upper_left = camera_center - vec3f{ 0.0, 0.0, focal_length } - viewport_u / two - viewport_v / two;
     const pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) / two;
 
+    const sphere = Sphere{
+        .center = vec3f{ 0.0, 0.0, -1.0 },
+        .radius = 0.5,
+    };
     for (0..image_height) |img_y| {
         for (0..image_width) |img_x| {
             const x_vf32 = @as(vec3f, @splat(@as(f32, @floatFromInt(img_x))));
             const y_vf32 = @as(vec3f, @splat(@as(f32, @floatFromInt(img_y))));
             const pixel_center = pixel00_loc + (x_vf32 * pixel_delta_u) + (y_vf32 * pixel_delta_v);
             const ray_direction = pixel_center - camera_center;
-            const ray = Ray{.dir = ray_direction, .origin = camera_center};
-            const color = ray_color(&ray);
+            const ray = Ray{ .dir = ray_direction, .origin = camera_center };
+            const color = ray_color(&ray, &sphere);
             const idx = 3 * (image_width * img_y + img_x);
 
-            img[idx+0] = @intFromFloat(255.0 * @max(@min(1.0, color[0]), 0.0));
-            img[idx+1] = @intFromFloat(255.0 * @max(@min(1.0, color[1]), 0.0));
-            img[idx+2] = @intFromFloat(255.0 * @max(@min(1.0, color[2]), 0.0));
+            img[idx + 0] = @intFromFloat(255.0 * @max(@min(1.0, color[0]), 0.0));
+            img[idx + 1] = @intFromFloat(255.0 * @max(@min(1.0, color[1]), 0.0));
+            img[idx + 2] = @intFromFloat(255.0 * @max(@min(1.0, color[2]), 0.0));
         }
     }
-
-    // launch_raycast(img.ptr, image_width, image_height);
 
     if (stbiw.stbi_write_png("output.png", image_width, image_height, 3, img.ptr, image_width * 3) == 0) {
         return error.ImageWriteFailed;
